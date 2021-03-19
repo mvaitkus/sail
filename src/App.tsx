bd1ba4f36173bfc17cb1930977e443cd0e1f0228
Marius Vaitkus <mariusv@wix.com>
2021-03-19 21:25:23 +0000

    Print sail sizes for 82 kg person

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <SailSizes weight={82} />
       </header>
     </div>
   );
 }
+
+interface SailSizesProps {
+  weight: number;
+}
+
+const SailSizes: React.FC<SailSizesProps> = props => {
+  const sailSizeForWind = (windStrengthKnots: number) =>
+    (1.34 * props.weight) / windStrengthKnots;
+
+  const interestingWindRange = Array.from(new Array(30), (x, i) => i + 10);
+  const sails = interestingWindRange.map(windKts => (
+    <WindAndSailSize
+      key={windKts}
+      windStrengthKts={windKts}
+      sailSize={sailSizeForWind(windKts)}
+    />
+  ));
+  return <div>{sails}</div>;
+};
+
+interface WindAndSailSizeProps {
+  windStrengthKts: number;
+  sailSize: number;
+}
+
+const WindAndSailSize: React.FC<WindAndSailSizeProps> = props => {
+  return (
+    <div>
+      {props.windStrengthKts} kts = {props.sailSize.toFixed(1)} m2
+    </div>
+  );
+};
 
 export default App;
