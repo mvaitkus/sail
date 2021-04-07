3262854fd3e30cf40a34d27e410fa90b235cf2fe
Marius Vaitkus <mariusv@wix.com>
2021-04-07 22:57:16 +0300

    Add recoil for state management with a sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,36 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { atom, RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <SailSizes weight={82} />
+        <RecoilRoot>
+          <SailSizes weight={82} />
+        </RecoilRoot>
       </header>
     </div>
   );
 }
+
+const windSpeedUnitState = atom({
+  key: "windSpeedUnit",
+  default: "kts"
+});
 
 interface SailSizesProps {
   weight: number;
 }
 
 const SailSizes: React.FC<SailSizesProps> = props => {
+  const windSpeedUnit = useRecoilValue(windSpeedUnitState)
   const sailSizeForWind = (windStrengthKnots: number) =>
     (1.34 * props.weight) / windStrengthKnots;
 
-  const interestingWindRange = Array.from(new Array(27), (x, i) => i + 12);
+  const interestingWindRange = Array.from(new Array(27), (_, i) => i + 12);
   const sails = interestingWindRange.map(windKts => (
     <WindAndSailSize
       key={windKts}
@@ -29,7 +38,27 @@ const SailSizes: React.FC<SailSizesProps> = props => {
       sailSize={sailSizeForWind(windKts)}
     />
   ));
-  return <div>{sails}</div>;
+  return (
+    <div>
+      <div>
+        <WindSpeedUnitSelector />
+      </div>
+      <div>Selected unit: {windSpeedUnit}</div>
+      {sails}
+    </div>
+  );
+};
+
+const WindSpeedUnitSelector: React.FC = () => {
+  const [, setWindSpeedUnit] = useRecoilState(windSpeedUnitState);
+  return (
+    <div>
+      <div>
+        <button onClick={() => setWindSpeedUnit("kts")}>kts</button>
+        <button onClick={() => setWindSpeedUnit("mps")}>m/s</button>
+      </div>
+    </div>
+  );
 };
 
 interface WindAndSailSizeProps {
