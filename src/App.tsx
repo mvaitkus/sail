909db0b7aa6192c0578eacb1f0f8eeeff6c90127
Marius Vaitkus <mariusv@wix.com>
2021-05-05 21:16:08 +0300

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+
 import { RecoilRoot } from "recoil";
 import { WindToSail } from "./WindToSail";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <RecoilRoot>
-          <WindToSail />
-        </RecoilRoot>
-      </header>
+      <RecoilRoot>
+        <Router>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className="App-menu">
+              <Link to="/" className="App-link">
+                Wind
+              </Link>
+              <Link to="/my" className="App-link">
+                My Sails
+              </Link>
+            </div>
+          </header>
+          <Switch>
+            <Route path="/my">
+              <div>My sails component</div>
+            </Route>
+            <Route path="/">
+              <WindToSail />
+            </Route>
+          </Switch>
+        </Router>
+      </RecoilRoot>
     </div>
   );
 }
